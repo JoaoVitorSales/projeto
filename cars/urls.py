20acdaeef8bf2249a37b77f655f596476e1d7808
Joao Vitor Sales <joao.0906@hotmail.com>
2023-07-04 00:53:58 -0300

    remove contato and sobre

--- cars/urls.py
+++ cars/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from cars.views import home, contato, sobre
+from cars.views import home
 
 urlpatterns = [
     path('', home),
     path('home/', home),
-    path('contato/', contato),
-    path('sobre/', sobre)
 ]
--- cars/views.py
+++ cars/views.py
@@ -5,10 +5,3 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 
-
-def contato(request):
-    return HttpResponse("contato")
-
-
-def sobre(request):
-    return HttpResponse("sobre")
