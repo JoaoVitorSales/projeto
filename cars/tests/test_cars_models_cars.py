59f2a4502ee1d6e2a4b3e16a2f133d0a2734ec05
Joao Vitor Sales <joao.0906@hotmail.com>
2023-07-13 01:29:23 -0300

    testing default values

--- cars/tests/test_cars_models_cars.py
+++ cars/tests/test_cars_models_cars.py
@@ -1,4 +1,6 @@
-from .test_cars_base import CarsViewTest
+from parameterized import parameterized
+from .test_cars_base import CarsViewTest, Cars
+from django.core.exceptions import ValidationError
 
 
 class CarsModelTest(CarsViewTest):
@@ -6,6 +8,24 @@ class CarsModelTest(CarsViewTest):
         self.car = self.make_car()
         return super().setUp()
 
-    def test_the_test(self):
-        car = self.car
-        ...
+    def make_models_not_default(self):
+        car = Cars(shop=self.make_shop(name='arrascaeta'),
+                   author=self.make_user(username='Gabigol'),
+                   title='car Title',
+                   details='car',
+                   slug='car-slug',
+                   value_unit='100',
+                   description='car Description')
+        self.car.full_clean()
+        self.car.save()
+        return car
+
+    @parameterized.expand([('title', 30), ('details', 100), ('value_unit', 8), ('description', 2000)])  # noqa
+    def test_the_cars_title_raises_error_if_title_has_more_than_max_letters(self, fields, max_lenght):  # noqa
+        setattr(self.car, fields, 'A' * (max_lenght + 1))
+        with self.assertRaises(ValidationError):
+            self.car.full_clean()
+
+    def test_if_is_published_was_false(self):
+        cars = self.make_models_not_default()
+        self.assertFalse(cars.is_published)
