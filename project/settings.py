1dcf86045c6f6a116b3a71100a26acbcbdd9adeb
JoaoVitorSales <picoletando@gmail.com>
2024-03-28 16:16:00 -0300

    Fix Server

--- project/settings.py
+++ project/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'debug_toolbar',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     # My Apps
     'cars',
     'authors',
@@ -94,12 +94,12 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
