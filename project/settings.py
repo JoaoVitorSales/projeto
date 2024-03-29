848ce23c7ebf1c7562599d8a6fcafbad2c212d5f
JoaoVitorSales <picoletando@gmail.com>
2024-03-29 01:09:04 -0300

    Fix LocalHost

--- project/settings.py
+++ project/settings.py
@@ -30,6 +30,7 @@ DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS: list[str] = ['*']
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -92,6 +93,12 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
